c9e88956fb265267e69f265f4e4c7dda5d49cc94
SergeyHist <sergeyhist@yahoo.com>
2022-08-31 20:30:51 +0300

    dk-1.2.1

--- trakt-dark-knight.user.js
+++ trakt-dark-knight.user.js
@@ -2,7 +2,7 @@
 // @name        Trak.tv Dark Knight
 // @namespace   https://github.com/sergeyhist/trakt-scripts/trakt-dark-knight.user.js
 // @match       *://trakt.tv/*
-// @version     1.2
+// @version     1.2.1
 // @author      Hist
 // @description Enable dark knight mode for trakt.tv
 // @run-at      document-start
@@ -10,6 +10,6 @@
 // @downloadURL https://github.com/sergeyhist/trakt-scripts/raw/main/trakt-dark-knight.user.js
 // ==/UserScript==
 
-setTimeout(() => {
-  !document.body.classList.contains('dark-knight') && document.body.classList.add('dark-knight');
+setInterval(() => {
+  document.body && !document.body.classList.contains('dark-knight') && document.body.classList.add('dark-knight');
 }, 100);
