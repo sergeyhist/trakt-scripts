08130c0c5a059eb20aac663bbb7d9b8a1e5dc769
SergeyHist <sergeyhist@yahoo.com>
2022-11-10 14:31:00 +0300

    fix

--- trakt-hidden.user.js
+++ trakt-hidden.user.js
@@ -2,7 +2,7 @@
 // @name        Trak.tv Hidden Items
 // @namespace   https://github.com/sergeyhist/trakt-scripts/trakt-hidden.user.js
 // @match       *://trakt.tv/*
-// @version     1.0
+// @version     1.1
 // @author      Hist
 // @description Add menu elements with hidden items
 // @run-at      document-start
@@ -11,23 +11,32 @@
 // ==/UserScript==
 
 const linkElements = ['#links-wrapper'];
-const listElements = ['#user-menu', '#mobile-nav']
+const listElements = ['#user-menu', '#mobile-nav'];
+
 
 addEventListener('DOMContentLoaded', () => {
   for (let element of linkElements) {
-    let link = document.createElement('a');
+    if (document.querySelector(element)) {
+      for (let item of document.querySelectorAll(element)) {
+        let link = document.createElement('a');
 
-    link.href = '/settings/hidden';
-    link.textContent = 'Hidden Items';
+        link.href = '/settings/hidden';
+        link.textContent = 'Hidden Items';
 
-    document.querySelector(element).querySelector('.container').append(link);
+        item.querySelector('.container').append(link);
+      };
+    };
   };
 
   for (let element of listElements) {
-    let li = document.createElement('li');
-    
-    li.innerHTML = `<a href="/settings/hidden">Hidden Items</a>`;
+    if (document.querySelector(element)) {
+      for (let item of document.querySelectorAll(element)) {
+        let li = document.createElement('li');
 
-    document.querySelector(element).querySelector('.separator') ? document.querySelector(element).querySelector('.separator').before(li) : document.querySelector(element).querySelector('ul').append(li);
+        li.innerHTML = `<a href="/settings/hidden">Hidden Items</a>`;
+
+        item.querySelector('.separator') ? item.querySelector('.separator').before(li) : item.querySelector('ul').append(li);
+      };
+    };
   };
 })
