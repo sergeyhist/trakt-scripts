57d49ff2572cb6f85aa51615a09b4a942a91d69e
SergeyHist <sergeyhist@yahoo.com>
2022-12-23 19:49:06 +0300

    trakt-adblock 1.3

--- trakt-adblock.user.js
+++ trakt-adblock.user.js
@@ -2,7 +2,7 @@
 // @name        Trak.tv AdBlock
 // @namespace   https://github.com/sergeyhist/trakt-scripts/trakt-adblock.user.js
 // @match       *://trakt.tv/*
-// @version     1.2
+// @version     1.3
 // @author      Hist
 // @description Block ads on trakt.tv
 // @run-at      document-start
@@ -10,21 +10,20 @@
 // @downloadURL https://github.com/sergeyhist/trakt-scripts/raw/main/trakt-adblock.user.js
 // ==/UserScript==
 
-addEventListener('DOMContentLoaded', () => {
-  const adsIds = ['.playwire', '.snigel'];
-  let adsNodes = [];
-
-  for (let id of adsIds) {
-    for (let element of document.querySelectorAll(id)) {
-      adsNodes.push(element);
-    };
+setInterval(() => {
+  for (let element of document.querySelectorAll('.snigel')) {
+    !element.parentElement.classList.contains('hidden') && element.parentElement.classList.add('hidden');
   };
 
-  for (let node of adsNodes) {
-    node.parentElement.classList.add('hidden');
+  for (let element of document.querySelectorAll('.playwire')) {
+    !element.parentElement.classList.contains('hidden') && element.parentElement.classList.add('hidden');
   };
 
-  for (let node of document.querySelectorAll('.replace-xlg')) {
-    node.classList.add('hidden');
+  for (let element of document.querySelectorAll('[class^="snigel-"]')) {
+    !element.classList.contains('hidden') && element.classList.add('hidden');
   };
-})
+
+  for (let element of document.querySelectorAll('.replace-xlg')) {
+    !element.classList.contains('hidden') && element.classList.add('hidden');
+  };
+}, 500)
